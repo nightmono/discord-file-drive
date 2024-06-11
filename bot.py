c2b3b2e0a052bb0a1207a263e77039a4cf007941
nightmono <163682003+nightmono@users.noreply.github.com>
2024-06-11 22:07:10 +0100

    Implement view file command As well as removed ctx out of download and save file functions

--- bot.py
+++ bot.py
@@ -13,32 +13,61 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="$", intents=intents)
 
-async def download_file(ctx: discord.ApplicationContext, file: discord.Attachment) -> bytes:
+def load_files():
+    global loaded_files
+
+    loaded_files = os.listdir("drive")
+    print(f"Loaded files:\n{loaded_files}")
+
+load_files()
+
+async def download_file(file: discord.Attachment) -> bytes:
     async with aiohttp.ClientSession() as session:
         async with session.get(file.url) as resp:
             if resp.status == 200:
                 return await resp.read()
             else:
-                await ctx.respond(f"Failed to download file: {file.filename}")
                 return None
 
-async def save_file(ctx: discord.ApplicationContext, filename: str, file_bytes: bytes):
+async def save_file(filename: str, file_bytes: bytes):
     async with aiofiles.open(f"drive/{filename}", "wb") as f:
         await f.write(file_bytes)
-    
-    await ctx.respond(f"Saved file: {filename}")
 
 @bot.slash_command(name="add", description="Add a file to the drive")
 @option("file", discord.Attachment, description="File to add to the drive",)
 async def add_file(ctx: discord.ApplicationContext, file: discord.Attachment): 
-    file_bytes = await download_file(ctx, file)
+    file_bytes = await download_file(file)
     if file_bytes is not None:
-        await save_file(ctx, file.filename, file_bytes)
+        await save_file(file.filename, file_bytes)
+        file_saved_reponse = await ctx.respond(f"Saved file: {file.filename}\nReloading file list...")
+        load_files()
+        await file_saved_reponse.edit_original_response(content=f"Saved file: {file.filename}\nFile list reloaded!")
+    else:
+        await ctx.respond(f"Failed to download file: {file.filename}")
 
-@bot.slash_command(name="view")
-@option("file", description="File to view. Image files are viewed with an embed.")
+async def read_file(filename: str) -> str:
+    try:
+        async with aiofiles.open(f"drive/{filename}", "r") as f:
+            return await f.read()
+    except FileNotFoundError:
+        return None
+
+async def get_files(ctx: discord.AutocompleteContext) -> list:
+    return loaded_files
+
+@bot.slash_command(name="view", description="View a file from the drive, image files are displayed using an embed.")
+@option("file", description="File to view.", autocomplete=get_files)
 async def view_file(ctx: discord.ApplicationContext, file: str): 
-    await ctx.respond("Not implemented yet.")
+    file_content = await read_file(file)
+
+    embed_title = f"Viewing file: {file}"
+    
+    if len(file_content) > 4093:
+        embed_description = f"```{file_content[:4043]}\n[Message over 4096 characters, thus truncated]```"
+    else:
+        embed_description = f"```{file_content}```"
+    
+    await ctx.respond(embed=discord.Embed(title=embed_title, description=embed_description))
 
 @bot.slash_command(name="remove")
 @option("file", description="File to remove")
